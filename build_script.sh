f2aac6710fbd20b7f12fc8773f106510e09522a9
Mozzie-jpg <56394771+Mozzie-jpg@users.noreply.github.com>
2020-10-25 21:01:45 +0100

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -4,7 +4,7 @@ pip3 install nuitka
 pip3 install hashlib
 clear
 echo "building binary..."
-python3 -m nuitka src/src.py
+python3 -m nuitka src/src.py --follow-imports
 rm -rf src.build
 mv src.bin pass_man
 clear
